a664bdc836223b36372237cfc7e0a5466a844fa7
AmjedKhaled165 <amjedkhaled115@gmail.com>
2026-03-18 17:29:54 +0200

    Fix 500 error on halan users API by fixing DB schema (avatar and courier_supervisors)

--- server/migrations/002_add_courier_supervisors.sql
+++ server/migrations/002_add_courier_supervisors.sql
@@ -6,12 +6,7 @@ CREATE TABLE IF NOT EXISTS courier_supervisors (
     PRIMARY KEY (courier_id, supervisor_id)
 );
 
--- Migrate existing data
-INSERT INTO courier_supervisors (courier_id, supervisor_id)
-SELECT id, supervisor_id
-FROM users
-WHERE supervisor_id IS NOT NULL
-ON CONFLICT DO NOTHING;
+
 
 -- Index for performance
 CREATE INDEX IF NOT EXISTS idx_courier_supervisors_courier ON courier_supervisors(courier_id);
